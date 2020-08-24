7f5549774667f8a3300de80c15fe29dec5934f69
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2020-08-24 11:52:43 -0500

    deployment commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,10 @@
 
 BlogPost.create(post_title: "Test Blog Post 1", post_content: "This is the test text for my first blog post", post_image_url: "fake_url.com")
 
-
 BlogPost.create(post_title: "Test Blog Post 2", post_content: "This is the test text for my second blog post", post_image_url: "fake_url.com")
 
 PortfolioProject.create(project_title: "Test Portfolio Project 1", project_summary: "This is the test summary for my first portfolio project", project_image_url: "fake_url.com", project_video_link: "fake_url.com", project_github_link: "fake_url.com", project_deploy_link: "fake_url.com")
 
 PortfolioProject.create(project_title: "Test Portfolio Project 2", project_summary: "This is the test summary for my second portfolio project", project_image_url: "fake_url.com", project_video_link: "fake_url.com", project_github_link: "fake_url.com", project_deploy_link: "fake_url.com")
+
+User.create(name: "Jess White", email: "jess_white.chicago@gmail.com", password: "MargaretDax15578", password_confirmation: "MargaretDax15578" )
