f2dde1a88e53013fcb1873e5db900d470ce4022e
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2020-08-24 11:40:58 -0500

    final commit pre-deploy

--- app/controllers/api/portfolio_projects_controller.rb
+++ app/controllers/api/portfolio_projects_controller.rb
@@ -2,7 +2,7 @@ class Api::PortfolioProjectsController < ApplicationController
 
   def index
     @portfolio_projects = PortfolioProject.all
-    @portfolio_projects = @portfolio_projects.order(id: :asc)
+    @portfolio_projects = @portfolio_projects.order(id: :desc)
     render "index.json.jb"
   end
 
@@ -13,7 +13,8 @@ class Api::PortfolioProjectsController < ApplicationController
       language: params[:language],
       project_image_url: params[:project_image_url],
       project_video_link: params[:project_video_link],
-      project_github_link: params[:project_github_link],
+      project_github_url_frontend: params[:project_github_url_frontend],
+      project_github_url_backend: params[:project_github_url_backend],
       project_deploy_link: params[:project_deploy_link]
       )
     if @portfolio_project.save
@@ -36,6 +37,8 @@ class Api::PortfolioProjectsController < ApplicationController
     @portfolio_project.project_image_url = params[:project_image_url] || @portfolio_project.project_image_url
     @portfolio_project.project_video_link = params[:project_video_link] || @portfolio_project.project_video_link
     @portfolio_project.project_github_link = params[:project_github_link] || @portfolio_project.project_github_link
+    @portfolio_project.project_github_url_frontend = params[:project_github_url_frontend] || @portfolio_project.project_github_url_frontend
+    @portfolio_project.project_github_url_backend = params[:project_github_url_backend] || @portfolio_project.project_github_url_backend
     @portfolio_project.project_deploy_link = params[:project_deploy_link] || @portfolio_project.project_deploy_link
     if @portfolio_project.save
       render "show.json.jb"
